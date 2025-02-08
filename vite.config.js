07dc6f73581c931576662b4991875f60961e2b25
BJ Blayney <bj.blayney@gmail.com>
2025-02-08 01:07:59 -0500

    adding gh-pages

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: '/french-verb-trainer/', // Set to repo name
+});
